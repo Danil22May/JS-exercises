0c5cf9fe7754569de71d8562c1d77e316e12d2f0
Danil22May <daniilmej88@gmail.com>
2024-06-26 23:51:42 +0200

    ex 12 done

--- ex11.12/app.js
+++ ex11.12/app.js
@@ -6,8 +6,32 @@
 const inputNum = document.getElementById("inputNum");
 const outputNum = document.getElementById("outputNum");
 
+const numbers = {
+  2: false,
+  3: false,
+  5: false,
+  7: false,
+};
+
 inputNum.addEventListener("input", () => {
   checkNumber();
 });
 
-const checkNumber = () => {};
+const checkNumber = () => {
+  let number = Number(inputNum.value);
+  console.log(typeof number);
+  if (number != 0) {
+    for (item in numbers) {
+      item = Number(item);
+      if (number % item == 0) {
+        numbers[item] = true;
+      }
+    }
+  } else {
+    for (item in numbers) {
+      numbers[item] = false;
+    }
+  }
+
+  outputNum.innerHTML = `Is divisible by 2: ${numbers[2]} | Is divisible by 3: ${numbers[3]} | Is divisible by 5: ${numbers[5]} | Is divisible by 7: ${numbers[7]}`;
+};
